e6c090dd4b1bea4cfa006a0087294631ec7cf578
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-04-19 16:37:47 +0300

    lil updates

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,21 +1,24 @@
 import { Container, Row, Col } from "react-bootstrap";
-import "./styles.css"
+import "./styles.css";
 
 import { Chatlist } from "../Chatlist";
 import { Messaging } from "../Messaging";
-
+import { useEffect } from "react";
 
 export const Layout = () => {
-    return (
-        <Container fluid className="layout">
-            <Row>
-                <Col xs={4} md={4} lg={3} className="p-0 pt-3 chatlist">
-                    <Chatlist />
-                </Col>
-                <Col className="p-0 pt-3 messages-container">
-                    <Messaging />
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  useEffect(() => {
+    document.title = "Whatsapp | Messages";
+  }, []);
+  return (
+    <Container fluid className="layout">
+      <Row>
+        <Col xs={4} md={4} lg={3} className="p-0 pt-3 chatlist">
+          <Chatlist />
+        </Col>
+        <Col className="p-0 pt-3 messages-container">
+          <Messaging />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
