62e6db9d36ed29c7ee04567e6ef56d9898b041c5
yeexng <118996751+yeexng@users.noreply.github.com>
2023-04-19 15:10:59 +0100

    Revert "added token auth to socketio"
    This reverts commit 3a08d911de967e6a5b83416224e94b54126ae98b.

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -9,33 +9,20 @@ import { disconnect } from "process";
 export const Layout = () => {
 
     const socket = io("http://localhost:3001", { transports: ['websocket'] }) 
-    const setUser = (token: string) => {
-        socket.emit("setUser", { token });
-        console.log(token)
-      };
 
-    useEffect(() => {
+    useEffect(()=>{
+        socket.on("userConnected",()=>{
+     
+     
+        })
+        socket.on('disconnect', () => {
+            console.log('Disconnected from socket');
+          })
 
-        socket.emit("userConnected", () => {
-            const token = localStorage.getItem("accessToken");
-            if (token) {
-              setUser(token);
-            }
-          });
-        socket.on("userConnected", () => {
-          console.log("Connected to socket");
-        });
-    
-       
-    
-        socket.on("disconnect", () => {
-          console.log("Disconnected from socket");
-        });
-    
         return () => {
-          socket.disconnect();
-        };
-      }, []);
+            socket.disconnect();
+          }
+    },[])
     return (
         <Container fluid className="layout">
             <Row>
